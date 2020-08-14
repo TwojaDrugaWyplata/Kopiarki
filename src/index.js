a91d71904e705d920d35d457bf0593ec0e9175fd
Paweł Szymczak <mangoman@wp.pl>
2020-08-14 17:46:04 +0200

    update project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
+import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import App from './components/Main';
+import About from './components/About';
+import LayoutWrapper from './components/LayoutWrapper';
+import User from './components/User';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Render the main component into the dom
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={LayoutWrapper}>
+      <IndexRoute component={App}/>
+      <Route path="/about" component={About} />
+      <Route path="/user/:userId" component={User}/>
+    </Route>
+  </Router>
+), document.getElementById('app'));
